37564f11a0d82ff1e1098e910e18d37cae33f809
Alexandre Nguyen <alex.nr@hotmail.fr>
2015-02-20 17:43:16 +0100

    Authentication

--- app.js
+++ app.js
@@ -13,4 +13,24 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
+app.post('/authenticate', function(req, res) {
+    
+    var admin_login = req.body.admin_login;
+    var admin_password = req.body.admin_password;
+
+    if(admin_login == config.values.admin_login
+        && admin_password == config.values.admin_password) {
+
+        res.redirect('/list');
+    }
+    else {
+        res.redirect('/');
+    }
+});
+
+app.get('/list', function(req, res) {
+
+    res.render('list');
+});
+
 app.listen(config.values.web_server_port);
--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 exports.values = {
     web_server_port: 3000,
     auth_server_port: 3001,
-    mongodb_addr: 'mongodb://localhost/track'
+    mongodb_addr: 'mongodb://localhost/track',
+    admin_login: 'admin',
+    admin_password: 'admin'
 };
