02e9bbac925348d0273ba87d2ea5d9e2b165b638
Alexandre Nguyen <alex.nr@hotmail.fr>
2015-02-20 17:56:23 +0100

    Manage session token

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 var config = require('./config');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var model = require('./model');
 var oauth = require('./oauth');
 
 app.use(bodyParser.json());
+app.use(session({secret: 'ssshhhhh'}));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -14,12 +17,14 @@ app.get('/', function (req, res) {
 });
 
 app.post('/authenticate', function(req, res) {
-    
+
     var admin_login = req.body.admin_login;
     var admin_password = req.body.admin_password;
 
     if(admin_login == config.values.admin_login
         && admin_password == config.values.admin_password) {
+
+        req.session.isConnected = true;
 
         res.redirect('/list');
     }
@@ -30,7 +35,22 @@ app.post('/authenticate', function(req, res) {
 
 app.get('/list', function(req, res) {
 
-    res.render('list');
+    if(req.session.isConnected) {
+
+        //Displaying all clients
+
+        model.ModelContainer.ClientModel.find({}, function(err, clients) {
+
+            res.render('list', clients);
+
+        });
+
+    }
+    else {
+
+        res.redirect('/');
+    }
+
 });
 
 app.listen(config.values.web_server_port);
