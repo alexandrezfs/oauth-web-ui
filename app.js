1652a6b8e858f44b140bb7b38315237987025440
Alexandre Nguyen <alex.nr@hotmail.fr>
2015-02-22 18:42:08 +0100

    Specified views folder

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(session({secret: 'ssshhhhh'}));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
     res.render('index');
